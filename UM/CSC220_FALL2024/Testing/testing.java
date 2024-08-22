67c2efe9dc86cf58ca1c7a92caa3f751847dc42b
zurdooo <a.ramos37@Hotmail.com>
2024-08-22 11:52:21 -0400

    java uses similar referencing to python

--- UM/CSC220_FALL2024/Testing/testing.java
+++ UM/CSC220_FALL2024/Testing/testing.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+
 public class testing {
 
     public static void main(String[] args) {
-        // Your code here
-        int x = 0;
-        System.out.println("4" + x);
+        // Initialize arrays
+        int[] test_1 = new int[5];
+        System.out.println("test_1: " + Arrays.toString(test_1));
+
+        int[] test_2 = new int[5];
+        test_2 = test_1;
+        test_2[1] = 5;
+        System.out.println("test_2: " + Arrays.toString(test_2));
+        System.out.println("test_1: " + Arrays.toString(test_1));
     }
 }
