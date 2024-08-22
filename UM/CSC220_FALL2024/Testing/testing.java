6e29deefeadf9ba46754c0b4fba2b0a5b9430d27
zurdooo <a.ramos37@Hotmail.com>
2024-08-22 11:26:50 -0400

    zed ass

--- UM/CSC220_FALL2024/Testing/testing.java
+++ UM/CSC220_FALL2024/Testing/testing.java
@@ -1,7 +1,8 @@
 public class testing {
+
     public static void main(String[] args) {
         // Your code here
-        System.out.println("4"+x);
-        
+        int x = 0;
+        System.out.println("4" + x);
     }
 }
