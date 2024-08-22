bdd958810b7ce08b4ed69dac304c5b5cdca1b08a
zurdooo <a.ramos37@Hotmail.com>
2024-08-22 14:53:56 -0400

    cloning java, similar reference to python

--- UM/CSC220_FALL2024/Testing/testing.java
+++ UM/CSC220_FALL2024/Testing/testing.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class testing {
 
+    // -- main
     public static void main(String[] args) {
         // Initialize arrays
         int[] test_1 = new int[5];
         System.out.println("test_1: " + Arrays.toString(test_1));
 
-        int[] test_2 = new int[5];
-        test_2 = test_1;
+        int[] test_2 = test_1.clone();
         test_2[1] = 5;
         System.out.println("test_2: " + Arrays.toString(test_2));
         System.out.println("test_1: " + Arrays.toString(test_1));
