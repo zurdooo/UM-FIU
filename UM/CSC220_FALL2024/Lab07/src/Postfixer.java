4350f4f19dac45b32e6407deaf5e6a50fef71322
zurdooo <a.ramos37@Hotmail.com>
2024-10-02 15:41:45 -0400

    lab 06  lab assignment done

---
+++ UM/CSC220_FALL2024/Lab07/src/Postfixer.java
@@ -0,0 +1,222 @@
+// package lab07;
+
+import java.util.*;
+
+public class Postfixer {
+
+    /**
+     * Converts an operator to its precedence priority
+     *
+     * We expect you to be able to handle +, -, *, /, ^, and ( (why don't you
+     * need ")" as well? see algorithm in part 4)
+     *
+     * The order of these is as follows: ^, * and /, + and -, (
+     *
+     * @param op a string representing an operator, e.g. "+" or "-"
+     * @return an integer value reflecting its precedence
+     */
+    private static int opToPrcd(String op) {
+        //DONE: Lab Part 2.1
+        int pre = switch (op) {
+            case "^" ->
+                4;
+            case "*", "/" ->
+                3;
+            case "+", "-" ->
+                2;
+            case "(" ->
+                1;
+            // Invalid
+            default ->
+                0;
+        };
+        return pre;
+    }
+
+    /**
+     * Determines if the first operator has same or greater precedence than the
+     * second
+     *
+     * @param op1 the first operator
+     * @param op2 the second operator
+     * @return the boolean result
+     */
+    private static boolean hasPrecedence(String op1, String op2) {
+        //DONE: Lab Part 2.2
+        // Get precedence for both then compare
+        return opToPrcd(op1) >= opToPrcd(op2);
+    }
+
+    /**
+     * determines if the input token is an operator
+     *
+     * @param token the string token to check
+     * @return a boolean reflecting the result
+     */
+    private static boolean isOperator(String token) {
+        //DONE: Lab Part 2.3
+        return switch (token) {
+            case "+", "-", "*", "/", "^" ->
+                true;
+            // All other cases
+            default ->
+                false;
+        };
+
+    }
+
+    /**
+     * Evaluates an expression
+     *
+     * NOTE Beware the order of pop and evaluation when receiving operand1 and
+     * operand2 as input.
+     *
+     * @param operand1 the first operand
+     * @param operator the operator to apply
+     * @param operand2 the second operand
+     * @return a double expressing the result
+     * @throws IllegalArgumentException if operator passed is not one of "+",
+     * "-", "*", "/", or "^"
+     *
+     */
+    private static double evaluate(double operand1, String operator, double operand2) {
+        //Done: Lab Part 2.4
+        // Check if operator is valid
+        if (!isOperator(operator)) {
+            throw new IllegalArgumentException("Invalid operator");
+        }
+        // Perform operation
+        return switch (operator) {
+            case "+" ->
+                operand1 + operand2;
+            case "-" ->
+                operand1 - operand2;
+            case "*" ->
+                operand1 * operand2;
+            case "/" ->
+                operand1 / operand2;
+            case "^" ->
+                Math.pow(operand1, operand2);
+            // Never accessed
+            default ->
+                0.0;
+        };
+    }
+
+    /**
+     * give a description of the purpose of this method
+     *
+     *
+     * @param line fill in
+     * @return fill in
+     */
+    public static double infixEvaluator(String line) {
+        //TODO: Lab Part 3
+        //HINT: You must use the algorithm described in Lab Part 4
+        StringSplitter data = new StringSplitter(line);
+        Stack<String> operators = new Stack<String>();
+        Stack<Double> operands = new Stack<Double>();
+
+        while (data.hasNext()) {
+            String token = data.next();
+            // Check if token is an operator
+            if (isOperator(token)) {
+                while (!operators.empty() && hasPrecedence(operators.peek(), token)) {
+                    // Get operator
+                    String operator = operators.pop();
+                    // Get operands
+                    double operand2 = operands.pop();
+                    double operand1 = operands.pop();
+                    // Push the evaluated result
+                    operands.push(evaluate(operand1, operator, operand2));
+                }
+                // Push the operator
+                operators.push(token);
+                // Right parenthesis
+            } else if (token.equals(")")) {
+                // Thing on top isn't left parenthesis
+                while (!operators.peek().equals("(")) {
+                    // Get operator
+                    String operator = operators.pop();
+                    // Get operands
+                    double operand2 = operands.pop();
+                    double operand1 = operands.pop();
+                    // Push the evaluated result
+                    operands.push(evaluate(operand1, operator, operand2));
+                }
+                // Discard left parenthesis
+                operators.pop();
+                // Left parenthesis
+            } else if (token.equals("(")) {
+                // Push the left parenthesis
+                operators.push(token);
+                // Number
+            } else {
+                operands.push(Double.parseDouble(token));
+            }
+        }
+        while (!operators.empty()) {
+            // Get operator
+            String operator = operators.pop();
+            // Get operands
+            double operand2 = operands.pop();
+            double operand1 = operands.pop();
+            // Push the evaluated result
+            operands.push(evaluate(operand1, operator, operand2));
+        }
+        // return last value in operands
+        return operands.pop();
+    }
+
+    /**
+     * give a description of the purpose of this method
+     *
+     * @param line fill in
+     * @return fill in
+     */
+    public static String toPostfix(String line) {
+        //TODO: Lab Part 4
+        return null; // placeholder
+    }
+
+    public static void main(String[] args) {
+
+        //Add Tests for Lab part 2
+        //opToPrcd(String op) Tests
+        //hasPrecedence(String op1, String op2) Tests
+        //isOperator(String token) Tests
+        //evaluate(double operand1, String operator, double operand2) Tests
+        //Uncomment when you are ready to test Lab Part 3
+        // if (infixEvaluator("10 + 2") != 12) {
+        //     System.err.println("test1 failed --> your answer should have been 12");
+        // }
+        // if (infixEvaluator("10 - 2 * 2 + 1") != 7) {
+        //     System.err.println("test1 failed --> your answer should have been 7");
+        // }
+        // if (infixEvaluator("100 * 2 + 12") != 212) {
+        //     System.err.println("test2 failed --> your answer should have been 212");
+        // }
+        // if (infixEvaluator("100 * ( 2 + 12 )") != 1400) {
+        //     System.err.println("test3 failed --> your answer should have been 1400");
+        // }
+
+        if (infixEvaluator("100 * ( 2 + 12 ) / 14") != 100) {
+            System.err.println("test4 failed --> your answer should have been 100");
+        }
+        System.out.println("Lab Testing Done!!!");
+
+        /* uncomment the below lines for assignment */
+        // if (!toPostfix(new String("(4+5)")).equals("45+"))
+        //     System.err.println("test1 failed --> should have been 45+");
+        // if (!toPostfix(new String("((4+5)*6)")).equals("45+6*"))
+        //     System.err.println("test2 failed --> should have been 45+6*");
+        // if (!toPostfix(new String("((4+((5*6)/7))-8)")).equals("456*7/+8-"))
+        //     System.err.println("test3 failed --> should have been 456*7/+8-");
+        // if (!toPostfix(new String("((4+5*(6-7))/8)")).equals("4567-*+8/"))
+        //     System.err.println("test4 failed --> should have been 4567-*+8/");
+        // if (!toPostfix(new String("(9+(8*7-(6/5^4)*3)*2)")).equals("987*654^/3*-2*+"))
+        //     System.err.println("test5 failed --> should have been 987*654^/3*-2*+");
+        // System.out.println("Assignment Testing Done!!!");
+    }
+
+}
---
+++ UM/CSC220_FALL2024/Lab07/src/StringSplitter.java
@@ -0,0 +1,109 @@
+// package lab07;
+
+import java.util.*;
+
+/**
+ * This class breaks up a string into a sequence of tokens using both whitespace and a list of special characters.
+ * The special characters in this case are used to tokenize an arithmetic expression. For example, 
+ * the expression "2*3.8/(4.95-7.8)" would be tokenized as "2", "*", "3.8", "/", "(", "4.95", "-", "7.8", ")" 
+ * even though it has no whitespace to separate these tokens.
+ * 
+ */
+public class StringSplitter {
+
+    // Queue to hold characters from the string being split
+    private Queue<Character> characters;
+
+    // Current token being processed
+    private String token;
+
+    // Special characters that act as delimiters for tokenization
+    public static final String SPECIAL_CHARACTER = "()+-*/^";
+
+    /**
+     * Constructs a new StringSplitter for the specified string.
+     * This constructor tokenize the input string by loading it into a queue
+     * and finding the first token.
+     * 
+     * @param line The string to be tokenized
+     */
+    public StringSplitter(String line) {
+        characters = new LinkedList<Character>();
+        for (int i = 0; i < line.length(); i++) {
+            characters.add(line.charAt(i));
+        }
+        findNextToken();
+    }
+
+    /**
+     * Returns true if there is another token in the string.
+     * 
+     * @return boolean true if there is another token, false otherwise
+     */
+    public boolean hasNext() {
+        return token != null;
+    }
+
+    /**
+     * Returns and consumes the next token in the string.
+     * 
+     * @return String representing the next token
+     * @throws NoSuchElementException if no token is available
+     */
+    public String next() {
+        checkToken();
+        String result = token;
+        findNextToken();
+        return result;
+    }
+
+    /**
+     * Returns the next token without consuming it.
+     * 
+     * @return String representing the next token
+     * @throws NoSuchElementException if no token is available
+     */
+    public String peek() {
+        checkToken();
+        return token;
+    }
+
+    /**
+     * Finds the next token from the string. Tokens are determined by either 
+     * being special characters or sequences of non-whitespace, non-special characters.
+     * This method is responsible for updating the `token` variable with the next available token.
+     */
+    private void findNextToken() {
+        while (!characters.isEmpty() && Character.isWhitespace(characters.peek())) {
+            characters.remove();
+        }
+        if (characters.isEmpty()) {
+            token = null;
+        } else {
+            token = "" + characters.remove();
+            if (!SPECIAL_CHARACTER.contains(token)) {
+                boolean done = false;
+                while (!characters.isEmpty() && !done) {
+                    char ch = characters.peek();
+                    if (Character.isWhitespace(ch) || SPECIAL_CHARACTER.indexOf(ch) >= 0) {
+                        done = true;
+                    } else {
+                        token = token + characters.remove();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Ensures that there is a token available. If no token is available, 
+     * this method throws a NoSuchElementException.
+     * 
+     * @throws NoSuchElementException if there are no tokens left
+     */
+    private void checkToken() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+    }
+}
